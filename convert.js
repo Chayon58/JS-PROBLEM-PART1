e20761bd819318702e3317cd2c23d39be3e1f6a5
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-15 13:07:53 +0600

    added convert

--- convert.js
+++ convert.js
@@ -28,3 +28,14 @@ function kiloMeterToMiles(kilo){
     const mile = kilo * 0.621371;
     return mile;
 }
+
+
+function inchToFeet2(inch) {
+    const feetFraction = inch / 12;
+    const feetNumber = parseInt(feetFraction);
+    const inchRemaining = inch % 12;
+    const result = feetNumber + ' ft ' + inchRemaining + ' inch.'
+    return result;
+}
+
+const shubhHeight = inchToFeet(75);
